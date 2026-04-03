9aad75c59756642180738ca528a4c0a9803dc193
超可爱的天禄 <3562644513@qq.com>
2026-04-03 23:59:30 +0800

    Add files via upload

---
+++ src/mod/content/TestProject.java
@@ -0,0 +1,24 @@
+package mod.content;
+
+import mindustry.world.Block;
+import mod.wored.blocks.TestItemAlpha;
+import mod.wored.blocks.TianLuRangeMiningMachine;
+import static mindustry.content.Items.*;
+import static mindustry.type.Category.*;
+import static mindustry.type.ItemStack.with;
+
+public class TestProject {
+    public static Block //注册方块
+        A1,B2;
+    public static void load(){//建筑
+        A1 = new TestItemAlpha("测试项A1") {{
+            requirements(crafting, with(copper, 999));
+            size = 4;
+        }};
+        B2 = new TianLuRangeMiningMachine("测试项B1") {{
+            requirements(crafting, with(null, 0));
+            size = 2;
+            range = 19;
+        }};
+    }
+}
